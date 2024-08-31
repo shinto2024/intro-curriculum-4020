80c176dc1510e250ac375beace588c2157771b43
shinto2024 <154789190+shinto2024@users.noreply.github.com>
2024-08-31 15:42:27 +0900

    feat: ３つの関数の結果をかけあわせた数値を、Promise チェーンを利用して出力するコードの実装

--- index.js
+++ index.js
@@ -18,5 +18,14 @@ function getC() {
   });
 }
 
-// TODO ここに getA, getB, getC で得られる結果をかけあわせた結果 2431 を標準出力するコードを記述する
-// ただし Promise チェイン(then関数の結果に対するthen関数の呼び出し)を一度は用いて実装をすること
+Promise.all([
+  getA(),
+  getB(),
+  getC(),
+]).then(values => {
+  let product = 1;
+  for (const value of values) {
+    product *= value;
+  }
+  console.log(product);
+});
